361b3d29a7b36cf7c520e64dbccba024a016091f
Blessed Sibanda <blasp.net@gmail.com>
2023-05-18 09:09:12 +0200

    Create .net console & msunit projects

---
+++ Averages.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Averages.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
